a923e9901a4b701b4121fbc34fc9e5aa04d899ed
Shamir León <leonshamir1@gmail.com>
2023-07-25 10:36:33 -0500

    Fix Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import Navigation from "./components/Navigation/Navigation"
+import Navigation from "./components/Navigation/Navigation";
+import PokeList from "./components/PokeList/PokeList";
+import SideMenu from "./components/SideMenu/SideMenu";
 
 function App() {
   return (
     <>
-      <Navigation />  
+      <Navigation />
+        <SideMenu />
+      <div className="Main-content">
+        <PokeList />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
