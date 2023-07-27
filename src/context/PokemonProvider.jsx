27aa6917f72a8b2b603f0eef0198de212066403f
Shamir León <leonshamir1@gmail.com>
2023-07-27 18:17:32 -0500

    Feat Filter Function

--- src/context/PokemonProvider.jsx
+++ src/context/PokemonProvider.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useState } from "react";
 import { PokemonContext } from "./PokemonContext";
 
-export const PokemonProvider = ({children}) => {
+export const PokemonProvider = ({ children }) => {
   const [allPokemons, setAllPokemons] = useState([]);
-  const [loading, setLoading] = useState(true)
+  const [globalPokemons, setGlobalPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [offset, setOffset] = useState(0);
 
-  const getAllPokemons = async () => {
-    const baseURL = "https://pokeapi.co/api/v2/";
+  const baseURL = "https://pokeapi.co/api/v2/";
 
-    const res = await fetch(`${baseURL}pokemon?limit=20&offset=300`);
+  /* Get first 50 15 pokemons */
+  const getAllPokemons = async (limit = 15) => {
+    const res = await fetch(
+      `${baseURL}pokemon?limit=${limit}&offset=${offset}`
+    );
     const data = await res.json();
 
     const promises = data.results.map(async (pokemon) => {
@@ -18,23 +23,64 @@ export const PokemonProvider = ({children}) => {
     });
 
     const results = await Promise.all(promises);
-    // console.log(results[1]);
 
     setAllPokemons([...allPokemons, ...results]);
-    setLoading(false)
+    setLoading(false);
+  };
+
+  /* Get global pokemons */
+  const getGlobalPokemons = async () => {
+    const res = await fetch(`${baseURL}pokemon?limit=10000&offset=${offset}`);
+    const data = await res.json();
+
+    const promises = data.results.map(async (pokemon) => {
+      const res = await fetch(pokemon.url);
+      const data = await res.json();
+      return data;
+    });
+    const results = await Promise.all(promises);
+
+    setGlobalPokemons(results);
+    setLoading(false);
   };
 
   useEffect(() => {
     getAllPokemons();
+  }, [offset]);
+
+  useEffect(() => {
+    getGlobalPokemons();
   }, []);
+
+  /* Load more button */
+  const loadMore = () => setOffset(offset + 15);
+
+  /* Filter pokemons */
+  const [filteredPokemons, setFilteredPokemons] = useState([]);
+
+  const filterPokemons = (nameType) => {
+    const filteredResults = globalPokemons.filter((pokemon) =>
+      pokemon.types.map((type) => type.type.name).includes(nameType)
+    );
+
+    setFilteredPokemons(filteredResults);
+    console.log(filteredPokemons);
+  };
 
   return (
     <PokemonContext.Provider
       value={{
         allPokemons,
+        globalPokemons,
         loading,
         setLoading,
+        loadMore,
+
+        filterPokemons,
+        filteredPokemons,
       }}
-    >{children}</PokemonContext.Provider>
+    >
+      {children}
+    </PokemonContext.Provider>
   );
 };
