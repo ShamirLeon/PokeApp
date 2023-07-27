9caeab5ca3a9e62c1a1db3538a4755e370efffb2
Shamir León <leonshamir1@gmail.com>
2023-07-27 18:20:05 -0500

    Feat filter pokemons logic

--- src/components/PokeList/PokeList.jsx
+++ src/components/PokeList/PokeList.jsx
@@ -4,17 +4,42 @@ import { PokemonContext } from "../../context/PokemonContext";
 import Loader from "../Loader/Loader";
 
 const PokeList = () => {
-  const { allPokemons, loading } = useContext(PokemonContext);
+  const { allPokemons, loading, loadMore, filteredPokemons } =
+    useContext(PokemonContext);
+
+  const bgColor1 = "#EAEAEA";
+  const bgColor2 = "#FFF";
 
   return (
     <>
       {loading ? (
-        <Loader></Loader>
+        <Loader />
       ) : (
         <main>
-          {allPokemons.map((pokemon) => (
-            <PokeItem pokemon={pokemon} key={pokemon.id} />
-          ))}
+          {filteredPokemons.length ? (
+            <>
+              {filteredPokemons.map((pokemon, index) => (
+                <PokeItem
+                  pokemon={pokemon}
+                  key={pokemon.id}
+                  color={index % 2 === 0 ? bgColor1 : bgColor2}
+                />
+              ))}
+            </>
+          ) : (
+            <>
+              {allPokemons.map((pokemon, index) => (
+                <PokeItem
+                  pokemon={pokemon}
+                  key={pokemon.id}
+                  color={index % 2 === 0 ? bgColor1 : bgColor2}
+                />
+              ))}
+            </>
+          )}
+          <button className="LoadMore" onClick={loadMore}>
+            Load More
+          </button>
         </main>
       )}
     </>
