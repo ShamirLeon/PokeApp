0fb2e602c348b99509f45538515c46feb9a5e608
Shamir León <leonshamir1@gmail.com>
2023-08-14 17:21:47 -0500

    clean filtered pokemons when page is created

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,8 +1,18 @@
 import { useLocation } from "wouter";
 import PokeList from "../components/PokeList/PokeList";
+import { useContext } from "react";
+import { PokemonContext } from "../context/PokemonContext";
+import { useEffect } from "react";
 
 const FavsPage = (params) => {
   const [location, setLocation] = useLocation();
+
+  const { getFavoritesPokemons, clearFilter } = useContext(PokemonContext)
+
+  useEffect(()=>{
+    clearFilter();
+    getFavoritesPokemons();
+  },[])
 
   return (
     <div className="Favs__Container">
