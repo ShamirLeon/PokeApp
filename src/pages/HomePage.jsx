a0ec2e74b41f0c595cd093b5ffd76d9036021bf8
Shamir León <leonshamir1@gmail.com>
2023-08-14 17:31:26 -0500

    clear filtered pokemons when home page starts

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,18 @@
 import { useLocation } from "wouter";
 import PokeList from "../components/PokeList/PokeList";
+import { useContext } from "react";
+import { PokemonContext } from "../context/PokemonContext";
+import { useEffect } from "react";
 
 const HomePage = () => {
   const [location, setLocation] = useLocation();
+
+  const { clearFilter } = useContext(PokemonContext);
+
+  useEffect(() => {
+    clearFilter();
+  }, []);
+
   return (
     <>
       <div className="Main-content">
